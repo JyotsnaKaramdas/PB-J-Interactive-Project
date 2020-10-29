dc2ecd0beada1e32905222a900edda3db764ccd4
jnyiu <71399772+alh642@users.noreply.github.com>
2020-10-29 16:04:22 -0500

    Anna's inital commit

--- sketch.js
+++ sketch.js
@@ -1,9 +1,21 @@
+// preload variables 
 let setting = {
   background: 'park',
   objects: ['blanket', 'basket'],
-  sandwich: ['bread', 'jelly', 'peanutButter','mustard','ketchup']
+  sandwich: ['bread', 'jelly', 'peanutButter']
 }
+
+// gradientConstants
+const Y_AXIS = 1;
+let b1, b2, c1, c2;
+
+// movingGradientVariables
+let x, y;
+let dy = 0;
+
+// falling foods
 let shapes = [];
+
 function preload() {
   setting.park = loadImage('images/park.png');
   setting.blanket = loadImage('images/blanket.png');
@@ -11,34 +23,47 @@ function preload() {
   setting.bread = loadImage('images/bread.png');
   setting.jelly = loadImage('images/jelly.png');
   setting.peanutButter = loadImage('images/peanutButter.png');
-  setting.mustard=loadImage('images/mustard.png');
-  setting.ketchup=loadImage('images/ketchup.png');
+  setting.mustard = loadImage('images/mustard.png');
+  setting.ketchup = loadImage('images/ketchup.png');
 
-scoreTracker=0;
-
-
-
+  scoreTracker = 0;
 }
+
 function setup() {
   createCanvas(600, 650);
   colorMode(HSB);
   angleMode(DEGREES);
+
+  // define gradientColors
+  c1 = color(285, 100, 30); // top color
+  c2 = color(195, 100, 100); // bottom color
+
+  // define movingGradientVariables 
+  x = 0;
+  y = -2900;
 }
+
 function draw() {
-  background(195, 100, 100);
-  image(setting.park, -100, 100, 770, 600);
-  image(setting.blanket, 50, 500, 500, 130);
-  image(setting.basket, 130, 520, 90, 80);
-  image(setting.bread, mouseX, 580, 125, 50);
+  // backgroundColorChanges 
+  speed = 0.25;
+  y = y + speed;
+  verticalGradient(0, y, width, height * 5, c1, c2, Y_AXIS);
+
+  // park setting images
+  image(setting.park, -100, 100 + frameCount * 0.25, 770, 600);
+  image(setting.blanket, 50, 500 + frameCount * 0.25, 500, 130);
+  image(setting.basket, 130, 520 + frameCount * 0.25, 90, 80);
+
+  // mouseX (bottom bread slice)
+  image(setting.bread, mouseX, 580 + frameCount * 0.25, 125, 50);
+
+  // score tracker properties 
   textSize(35);
-  textFont('Times New Roman')
-  text('SCORE: ',410,35);
-  text(scoreTracker,550,35);
+  textFont('Times New Roman');
+  text('SCORE:', 410, 35);
+  text(scoreTracker, 550, 35);
 
-  var d=dist
-
-
-
+  // falling foods for-loop
   for(let shape of shapes) {
     push();
     shape.y += 1.5;
@@ -46,36 +71,51 @@ function draw() {
       image(setting.bread, shape.x, shape.y, 125, 50);
     }
     if(shape.shapeType === 'jelly') {
-      image(setting.jelly, shape.x, shape.y, 165, 55);
+      image(setting.jelly, shape.x, shape.y, 125, 50);
     }
     if(shape.shapeType === 'peanutButter') {
-      image(setting.peanutButter, shape.x, shape.y, 165, 55);
+      image(setting.peanutButter, shape.x, shape.y, 125, 50);
     }
     if(shape.shapeType === 'mustard') {
-      image(setting.mustard, shape.x, shape.y, 165, 55);
-
+      image(setting.mustard, shape.x, shape.y, 125, 50);
     }
     if(shape.shapeType === 'ketchup') {
-      image(setting.ketchup, shape.x, shape.y, 165, 55);
+      image(setting.ketchup, shape.x, shape.y, 125, 50);
+  }
+    pop();
   }
 }
+
+function verticalGradient(x, y, w, h, c1, c2, axis) {
+  noFill();
+
+  if (axis === Y_AXIS) {
+    for (let i = y; i <= y + h; i ++) {
+      let inter = map(i, y, y + h, 0, 1);
+      let c = lerpColor(c1, c2,inter);
+      stroke(c);
+      line(x, i, x + w, i);
+    }
+  }
 }
+
+// keyboard interaction for falling objects 
 function keyPressed() {
   if(keyCode === SHIFT) {
     addAFood();
   }
 }
+
 function addAFood() {
   shape1 = {
     x: mouseX,
     y: -20,
-    shapeType: random(['bread', 'jelly', 'peanutButter','mustard','ketchup'])
+    shapeType: random(['bread', 'jelly', 'peanutButter', 'mustard', 'ketchup'])
   }
   shapes.push(shape1);
 }
+
+// mouse interaction for score tracker
 function mousePressed() {
-  
-  
   scoreTracker++;
 }
-
