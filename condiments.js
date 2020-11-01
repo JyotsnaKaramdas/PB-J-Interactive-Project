2052b9486df8e18663ea8b852d2f030522aab1e4
jnyiu <71399772+alh642@users.noreply.github.com>
2020-11-01 14:22:13 -0600

    in constructor added width & height

--- condiments.js
+++ condiments.js
@@ -7,89 +7,99 @@ function condiments() {
       
   function addAFood() {
     if (random(['bread', 'jelly', 'peanutButter', 'mustard', 'ketchup']) == 'bread') {
-      let food1 = new Bread(random(width), -20);
+      let food1 = new Bread(random(width), -20, 125, 50);
       foods.push(food1);
     }
     if (random(['bread', 'jelly', 'peanutButter', 'mustard', 'ketchup']) == 'jelly') {
-      let food1 = new Jelly(random(width), -20);
+      let food1 = new Jelly(random(width), -20, 125, 50);
       foods.push(food1);
     }
     if (random(['bread', 'jelly', 'peanutButter', 'mustard', 'ketchup']) == 'peanutButter') {
-      let food1 = new PeanutButter(random(width), -20);
+      let food1 = new PeanutButter(random(width), -20, 125, 50);
       foods.push(food1);
     }
     if (random(['bread', 'jelly', 'peanutButter', 'mustard', 'ketchup']) == 'mustard') {
-      let food1 = new Mustard(random(width), -20);
+      let food1 = new Mustard(random(width), -20, 125, 50);
       foods.push(food1);
     }
     if (random(['bread', 'jelly', 'peanutButter', 'mustard', 'ketchup']) == 'ketchup') {
-      let food1 = new Ketchup(random(width), -20);
+      let food1 = new Ketchup(random(width), -20, 125, 50);
       foods.push(food1);
     }
   }
       
   class Bread {
-    constructor(x, y) {
+    constructor(x, y, w, h) {
       this.x = x;
       this.y = y;
+      this.width = w;
+      this.height = h;
     }
     update() {
-      this.y += 1.25;
+      this.y += 2.5;
     }
     draw() {
-      image(setting.bread, this.x, this.y, 125, 50);
+      image(setting.bread, this.x, this.y, this.width, this.height);
     }
   } 
        
   class Jelly {
-    constructor(x, y) {
+    constructor(x, y, w, h) {
       this.x = x;
       this.y = y;
+      this.width = w;
+      this.height = h;
     }
     update() {
-      this.y += 1.25;
+      this.y += 2.5;
     } 
     draw() {
-      image(setting.jelly, this.x, this.y, 125, 50);
+      image(setting.jelly, this.x, this.y, this.width, this.height);
     }
   }
        
   class PeanutButter {
-    constructor(x, y) {
+    constructor(x, y, w, h) {
       this.x = x;
       this.y = y;
+      this.width = w;
+      this.height = h;
     }
     update() {
-      this.y += 1.25;
+      this.y += 2.5;
     }
     draw() {
-      image(setting.peanutButter, this.x, this.y, 125, 50);
+      image(setting.peanutButter, this.x, this.y, this.width, this.height);
     }
   }
       
   class Mustard {
-    constructor(x, y) {
+    constructor(x, y, w, h) {
       this.x = x;
       this.y = y;
+      this.width = w;
+      this.height = h;
     }
     update() {
-      this.y += 1.25;
+      this.y += 2.5;
     }
     draw() {
-      image(setting.mustard, this.x, this.y, 125, 50);
+      image(setting.mustard, this.x, this.y, this.width, this.height);
     }
   }
       
   class Ketchup {
-    constructor(x, y) {
+    constructor(x, y, w, h) {
       this.x = x;
       this.y = y;
+      this.width = w;
+      this.height = h;
     }
     update() {
-      this.y += 1.25;
+      this.y += 2.5;
     }
     draw() {
-      image(setting.ketchup, this.x, this.y, 125, 50);
+      image(setting.ketchup, this.x, this.y, this.width, this.height);
     }
   }
 }
