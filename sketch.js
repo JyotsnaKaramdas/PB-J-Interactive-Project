33d7b3941fdc6c89f8d8a490e3bf7e244123ec1c
JyotsnaKaramdas <54827861+JyotsnaKaramdas@users.noreply.github.com>
2020-11-05 15:06:04 -0500

    deleted code

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@
 let setting = {
   background: 'park',
   objects: ['blanket', 'basket'],
-  sandwich: ['bread', 'jelly', 'peanutButter'],
+  sandwich: 'bread',
   player: 'pbj'
 }
 
@@ -18,17 +18,18 @@ let dy = 0;
 let sandwich;
 
 // falling jelly array
-let jellies = [];
-let peanuts = [];
+let condiments=[];
+
+var c=null;
 
 function preload() {
   setting.park = loadImage('images/park.png');
   setting.blanket = loadImage('images/blanket.png');
   setting.basket = loadImage('images/basket.png');
   setting.bread = loadImage('images/bread.png');
-  setting.jelly = loadImage('images/jelly.png');
-  setting.peanutButter = loadImage('images/peanutButter.png');
   setting.pbj = loadImage('images/pbj.png');
+  
+  
 }
 
 function setup() {
@@ -45,16 +46,28 @@ function setup() {
   y = -2900;
 
   // jelly for-loop
-  for (let i = 0; i < 50; i += 5) {
-    jellies[i] = new Jelly(random(width), random(height), 60, 20);
+
+  for (i=0; i<2;i++) {
+    if (condiments!=c) {
+      condiments[i]= new Condiments()
+    }
+    
   }
-  // jelly for-loop
-  for (let i = 0; i < 50; i += 5) {
-    peanuts[i] = new PeanutButter(random(width), random(height), 50, 20);
-}
+
+  // for (let i = 0; i < 50; i += 5) {
+    // // jellies[i] = new Jelly(random(width), random(height), 60, 20);
+    // // peanuts[i] = new PeanutButter(random(width), random(height), 50, 20);
+    // condiments[0]= new Jelly(random(width), random(height), 60, 20);
+    // condiments[1]=new PeanutButter(random(width), random(height), 50, 20);
+  
+
+
+
   // player = sandwich
-  sandwich = new Sandwich(200, 90);
+  sandwich = new Sandwich(random(width), random(height),200,90);
+
 }
+
 
 function draw() {
   // backgroundColorChanges
@@ -67,20 +80,30 @@ function draw() {
   image(setting.blanket, 50, 500 + frameCount * 0.005, 500, 130);
   image(setting.basket, 130, 520 + frameCount * 0.005, 90, 80);
 
+  // player = sandwich 
+  sandwich.display(mouseX, mouseY,200,90);
+  
+  
+  
   // jelly for-loop
-  for (let i = 0; i < jellies.length; i += 5) {
-    jellies[i].update();
-    jellies[i].display();
+  for (let i = 0; i < condiments.length; i += 5) {
+    condiments[i].update();
+    condiments[i].display();
+    //  condiments[i].overlaps();
+    //  condiments[i].touches();
   }
 
-  // peanut butter for-loop
-  for (let i = 0; i < peanuts.length; i += 5) {
-      peanuts[i].update();
-      peanuts[i].display();
-    }
+  // // peanut butter for-loop
+  // for (let i = 0; i < peanuts.length; i += 5) {
+  //     peanuts[i].update();
+  //     peanuts[i].display();
+  //     peanuts[i].overlaps();
+  //     peanuts[i].touches();
+  //   }
 
-  // player = sandwich 
-  sandwich.display(mouseX, mouseY);
+  
+
+
 }
 
 function verticalGradient(x, y, w, h, c1, c2, axis) {
@@ -95,85 +118,178 @@ function verticalGradient(x, y, w, h, c1, c2, axis) {
   }
 }
 
-class Jelly {
-  constructor(x, y, w, h) {
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
+
+
+// function mousePressed() {
+
+// }
+
+// function changeImage(){
+//   if (condiments[i].overlaps() && jellies[i].touches()==true){
+//     jellies=image(setting.pbj, this.x1, this.y1, this.x2, this.y2);
+//   } else{
+//     jellies=jellies
+//   }
+  
+// }
+
+
+class Condiments {
+  constructor(x1, y1, x2, y2) {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+
+    this.r
   }
-  intersects(sandwich) {
-    let d = dist(this.x, this.y, sandwich.x, sandwich.y);
-    if (d < (this.x + this.w) + sandwich.x) {
-      return false;
-    }
-    if (d < this.x + (sandwich.x + sandwich.w)) {
-      return false;
-    }
-    if (d < ) {
-      return false;
-    }
-    if (d < ) {
-      return false;
-    } else {
-      return true;
-    }
-  }
+
+
+    //this.r= floor(random(0,condiments.length));
+
+
+  //}
+//   overlaps(sandwich) {
+    
+//     //no horizontal overlap
+//     if (this.x1>=sandwich.x2 || sandwich.x1 >=this.x2){
+//       return false;
+//     }
+
+//     //no vertical overlap
+//     if (this.y1 >= sandwich.y2 || sandwich.y2 >=this.y2) {
+//       return false;
+//     } else {
+//       return true;
+//     }
+//   }
+  
+//   touches (sandwich) {
+//     //has horizontal gap
+    
+//     if (this.x1>sandwich.x2 || sandwich.x1 >this.x2) {
+//       return false;
+//     }
+//     //has vertical gap
+
+//     if (this.y1>sandwich.y2 || sandwich.y1> this.y2) {
+//       return false;
+//     } else{
+//       return true;
+
+//    }
+
+//   }
+
+    
+  
+  
+  
+  
+  
+//     //   let d = dist(this.x, this.y, sandwich.x, sandwich.y);
+//   //   if (d < (this.x + this.w) + sandwich.x) {
+//   //     return false;
+//   //   }
+//   //   if (d < this.x + (sandwich.x + sandwich.w)) {
+//   //     return false;
+//   //   }
+//   //   if (d < ) {
+//   //     return false;
+//   //   }
+//   //   if (d < ) {
+//   //     return false;
+//   //   } else {
+//   //     return true;
+//   //   }
+//   // }
   display() {
-    image(setting.jelly, this.x, this.y, this.w, this.h);
+    image(condiments[i], this.x1, this.y1, this.x2, this.y2);
   }
   update() {
-    this.y = this.y += (1.5);
-    if (this.y > height){ 
-			  this.y = -this.h;
+    this.y1 = this.y1 += (1.5);
+    if (this.y1 > height){ 
+			  this.y1 = -this.y2;
 		}
   }
 }
-class PeanutButter {
-  constructor(x, y, w, h) {
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-  }
- intersects(sandwich) {
-    let d = dist(this.x, this.y, sandwich.x, sandwich.y);
-    if (d < this.x && this.h + sandwich.w && sandwich.h) {
-      return false;
-    } 
-    if (d < ) {
-      return false;
-    }
-    if (d <) {
-      return false;
-    }
-    if (d <) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-  display() {
-    image(setting.peanutButter, this.x, this.y, this.w, this.h);
-  }
-  update() {
-    this.y = this.y += (1.5);
-    if (this.y > height){ 
-        this.y = -this.h;
-    }
-  }
-}
+// class PeanutButter {
+//   constructor(x1, y1, x2,y2) {
+//     this.x1 = x1;
+//     this.y1 = y1;
+//     this.x2 = x2;
+//     this.y2 = y2;
+//   }
+//  overlaps(sandwich) {
+//   if (this.x1>=sandwich.x2 || sandwich.x1 >=this.x2){
+//     return false;
+//   }
+
+//   //no vertical overlap
+//   if (this.y1 >= sandwich.y2 || sandwich.y2 >=this.y2) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+
+//  }
+
+//  touches(sandwich){
+
+//   if (this.x1>sandwich.x2 || sandwich.x1 >this.x2) {
+//     return false;
+//   }
+//   //has vertical gap
+
+//   if (this.y1>sandwich.y2 || sandwich.y1> this.y2) {
+//     return false;
+//   } else{
+//     return true;
+
+//  }
+
+
+//  }
+ 
+ //   let d = dist(this.x, this.y, sandwich.x, sandwich.y);
+  //   if (d < this.x && this.h + sandwich.w && sandwich.h) {
+  //     return false;
+  //   } 
+  //   if (d < ) {
+  //     return false;
+  //   }
+  //   if (d <) {
+  //     return false;
+  //   }
+  //   if (d <) {
+  //     return false;
+  //   } else {
+  //     return true;
+  //   }
+  // }
+//   display() {
+//     image( this.x1, this.y1, this.x2, this.y2);
+//   }
+//   update() {
+//     this.y1 = this.y1 += (1.5);
+//     if (this.y1 > height){ 
+//         this.y1 = -this.y2;
+//     }
+//   }
+//  }
  
 class Sandwich {
-  constructor(w, h) {
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
+  constructor(x1,y1,x2,y2) {
+    this.x1 = x1;
+    this.y1= y1;
+    this.x2 = x2;
+    this.y2 = y2;
   }
-  display(x, y) {
-    this.x = x;
-    this.y = y;
-    image(setting.pbj, this.x, this.y, this.w, this.h);
+  display(x1, y1,x2,y2) {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2=x2;
+    this.y2=y2;
+    image(setting.pbj, this.x1, this.y1, this.x2, this.y2);
   }
 }
