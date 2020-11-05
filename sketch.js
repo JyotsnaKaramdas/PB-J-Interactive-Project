22615e94456c8a20c32f5d0c8b41370b06444f42
jnyiu <71399772+alh642@users.noreply.github.com>
2020-11-05 14:08:58 -0600

    back to old changes

--- sketch.js
+++ sketch.js
@@ -5,22 +5,17 @@ let setting = {
   sandwich: ['bread', 'jelly', 'peanutButter'],
   player: 'pbj'
 }
-
 // gradientConstants
 const Y_AXIS = 1;
 let b1, b2, c1, c2;
-
 // movingGradientVariables
 let x, y;
 let dy = 0;
-
 // player = sandwich
 let sandwich;
-
 // falling jelly array
 let jellies = [];
 let peanuts = [];
-
 function preload() {
   setting.park = loadImage('images/park.png');
   setting.blanket = loadImage('images/blanket.png');
@@ -30,16 +25,13 @@ function preload() {
   setting.peanutButter = loadImage('images/peanutButter.png');
   setting.pbj = loadImage('images/pbj.png');
 }
-
 function setup() {
   createCanvas(600, 650);
   colorMode(HSB);
   angleMode(DEGREES);
-
   // define gradientColors
   c1 = color(285, 100, 30); // top color
   c2 = color(195, 100, 100); // bottom color
-
   // define movingGradientVariables
   x = 0;
   y = -2900;
@@ -48,41 +40,27 @@ function setup() {
   for (let i = 0; i < 50; i += 5) {
     jellies[i] = new Jelly(random(width), random(height), 60, 20);
   }
-  // jelly for-loop
-  for (let i = 0; i < 50; i += 5) {
-    peanuts[i] = new PeanutButter(random(width), random(height), 50, 20);
-}
-  // player = sandwich
-  sandwich = new Sandwich(200, 90);
-}
 
+  // player = sandwich
+  sandwich = new Sandwich(100, 100, 200, 90);
+}
 function draw() {
   // backgroundColorChanges
   speed = 0.25;
   y = y + speed;
   verticalGradient(0, y, width, height * 5, c1, c2, Y_AXIS);
-
   // park setting images
   image(setting.park, -100, 100 + frameCount * 0.005, 770, 600);
   image(setting.blanket, 50, 500 + frameCount * 0.005, 500, 130);
   image(setting.basket, 130, 520 + frameCount * 0.005, 90, 80);
-
   // jelly for-loop
   for (let i = 0; i < jellies.length; i += 5) {
     jellies[i].update();
     jellies[i].display();
   }
-
-  // peanut butter for-loop
-  for (let i = 0; i < peanuts.length; i += 5) {
-      peanuts[i].update();
-      peanuts[i].display();
-    }
-
-  // player = sandwich 
+  // player = sandwich
   sandwich.display(mouseX, mouseY);
 }
-
 function verticalGradient(x, y, w, h, c1, c2, axis) {
   noFill();
   if (axis === Y_AXIS) {
@@ -94,7 +72,6 @@ function verticalGradient(x, y, w, h, c1, c2, axis) {
     }
   }
 }
-
 class Jelly {
   constructor(x, y, w, h) {
     this.x = x;
@@ -102,70 +79,19 @@ class Jelly {
     this.w = w;
     this.h = h;
   }
-  intersects(sandwich) {
-    let d = dist(this.x, this.y, sandwich.x, sandwich.y);
-    if (d < (this.x + this.w) + sandwich.x) {
-      return false;
-    }
-    if (d < this.x + (sandwich.x + sandwich.w)) {
-      return false;
-    }
-    if (d < ) {
-      return false;
-    }
-    if (d < ) {
-      return false;
-    } else {
-      return true;
-    }
-  }
   display() {
     image(setting.jelly, this.x, this.y, this.w, this.h);
   }
   update() {
     this.y = this.y += (1.5);
-    if (this.y > height){ 
-			  this.y = -this.h;
-		}
-  }
-}
-class PeanutButter {
-  constructor(x, y, w, h) {
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-  }
- intersects(sandwich) {
-    let d = dist(this.x, this.y, sandwich.x, sandwich.y);
-    if (d < this.x && this.h + sandwich.w && sandwich.h) {
-      return false;
-    } 
-    if (d < ) {
-      return false;
-    }
-    if (d <) {
-      return false;
-    }
-    if (d <) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-  display() {
-    image(setting.peanutButter, this.x, this.y, this.w, this.h);
-  }
-  update() {
-    this.y = this.y += (1.5);
-    if (this.y > height){ 
+    if (this.y > height){
         this.y = -this.h;
     }
   }
 }
- 
+
 class Sandwich {
-  constructor(w, h) {
+  constructor(x, y, w, h) {
     this.x = x;
     this.y = y;
     this.w = w;
